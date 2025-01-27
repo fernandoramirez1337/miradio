efac1693bfe6aeb3960a76db136d0e919bea2996
99 <feeer999@protonmail.com>
2025-01-27 00:00:53 -0500

    Initial Commit

---
+++ miradio/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  miradio
+//
+//  Created by traintagger on 27/01/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
